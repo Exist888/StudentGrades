b9f81fefb6d9566067688e46cf0f75c0456d48ab
Filip Herbst <filip_herbst@yahoo.com>
2024-09-02 21:11:27 -0500

    Commit 4 - populating student list with each instance

--- Module2Review-StudentGrades/Program.cs
+++ Module2Review-StudentGrades/Program.cs
@@ -9,31 +9,35 @@ class Program
         //List of Type "Student"
         var students = new List<Student>();
 
-        //4 Instances of Type "Student"
+        //4 Instances of Type "Student", populating Student List with each instance
         var student1 = new Student()
         {
             ID = 7854935,
             Name = "Frodo Baggins",
         };
+        students.Add(student1);
 
         var student2 = new Student()
         {
             ID = 7854936,
             Name = "Samwise Gamgee",
         };
+        students.Add(student2);
 
         var student3 = new Student()
         {
             ID = 7854937,
             Name = "Peregrin Took",
         };
+        students.Add(student3);
 
         var student4 = new Student()
         {
             ID = 7854938,
             Name = "Pippin Took",
         };
-        
+        students.Add(student4);
+
     }
 
 
