9969d501400c0183fd85df664cb1857857a718c4
Filip Herbst <filip_herbst@yahoo.com>
2024-09-02 20:15:25 -0500

    Commit 2 - added method to calculate grade average

--- Module2Review-StudentGrades/Student.cs
+++ Module2Review-StudentGrades/Student.cs
@@ -2,23 +2,41 @@
 
 public class Student
 {
-    //Properties of Class "Student"
+    //Properties of Class "Student" (Name and ID)
     public string Name { get; set; }
     public int ID { get; set; }
     
-    //Grades List (property)
+    //List as Property (List of Grades)
     public static List<double> Grades { get; set; } = new List<double>();
 
     
-    //Method to add student grade to Grades List
+    //Method to add individual student grade to Grades List
     public static void AddGrade(double grade)
     {
         Grades.Add(grade);
     }
     
-    //Method to add student grades to Grades List
-    public static void AddGrade(double[] grades)
+    //Method to add multiple grades (array) to Grades List
+    public static void AddGrade(params double[] grades)
     {
         Grades.AddRange(grades);
     }
+    
+    //Method to calculate Grade Average from Grades List
+    public static double CalculateAverageGrade()
+    {
+        if (Grades == null)
+        {
+            return 0;
+        }
+        else
+        {
+            return Grades.Sum() / Grades.Count;
+        }
+        
+        // double gradesSum = Grades.Sum();
+        // double gradesAvg = gradesSum / Grades.Count;
+        // return gradesAvg;
+    }
+    
 }
